23d9d415b911df47fedf0f0659b04dfa2b75d6f1
lolrow <owen.strader2@gmail.com>
2024-04-03 18:31:41 -0500

    Added sv_yaw to config for easier movement updates and testing, added command to set yaw in game, hardcoded the config settings of the server into the mod.

--- src/main/java/net/nerdorg/minehop/config/MinehopConfig.java
+++ src/main/java/net/nerdorg/minehop/config/MinehopConfig.java
@@ -8,7 +8,7 @@ public class MinehopConfig implements ConfigData {
     public double sv_friction = 0.35;  // 0.5 default
     public double sv_accelerate = 0.1;  // 0.1 default
     public double sv_airaccelerate = 1.0E99;  // 0.2 default
-    public double sv_maxairspeed = 0.4015;  // 0.1 default
+    public double sv_maxairspeed = 0.04015;  // 0.1 default
     public double speed_mul = 2.2;  // 2.2 default
     public double sv_gravity = 0.066;  // 0.08 default
     public double sv_yaw = 8.9; // 10.25f default
