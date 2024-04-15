b7518d4b4289bf1bbc164dcff316a7b789d8d262
Plaaasma <47513553+Plaaasma@users.noreply.github.com>
2024-04-15 01:52:19 -0500

    Changed map menu to filter for map type

--- src/client/java/net/nerdorg/minehop/screen/SelectMapScreen.java
+++ src/client/java/net/nerdorg/minehop/screen/SelectMapScreen.java
@@ -6,6 +6,8 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.CyclingButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -16,10 +18,18 @@ import net.nerdorg.minehop.screen.widget.MapListWidget;
 
 @Environment(EnvType.CLIENT)
 public class SelectMapScreen extends Screen {
-    private MapListWidget listWidget;
-    private TextFieldWidget  textFieldWidget;
+    private MapListWidget bhopListWidget;
+    private MapListWidget arenaListWidget;
+    private MapListWidget hnsListWidget;
+    private TextFieldWidget textFieldWidget;
+    private ButtonWidget bhopButtonWidget;
+    private ButtonWidget arenaButtonWidget;
+    private ButtonWidget hnsButtonWidget;
+
     private String lastFieldText = "";
 
+    private boolean bhop_tab = true;
+    private boolean arena_tab = false;
 
     public SelectMapScreen(Text title) {
         super(title);
@@ -31,23 +41,101 @@ public class SelectMapScreen extends Screen {
         this.textFieldWidget = new TextFieldWidget(this.client.textRenderer, (this.width / 2) - (128 / 2), 16, 128, 14, Text.literal("Map Filter"));
         this.addSelectableChild(this.textFieldWidget);
 
-        this.listWidget = new MapListWidget(this.client, this.width, this.height - 32, 32, 20);
-        for (DataManager.RecordData recordData : Minehop.recordList) {
-            if (!recordData.map_name.equals("spawn")) {
+        this.bhopButtonWidget = ButtonWidget.builder(Text.of("Bhop"), (press) -> {
+                    bhop_tab = true;
+                    arena_tab = false;
+                    this.hnsButtonWidget.active = true;
+                    this.hnsListWidget.active = false;
+                    this.bhopButtonWidget.active = false;
+                    this.bhopListWidget.active = true;
+                    this.arenaButtonWidget.active = true;
+                    this.arenaListWidget.active = false;
+                })
+                .size(48, 12)
+                .build();
+
+        this.bhopButtonWidget.setX((this.width / 2) - (this.bhopButtonWidget.getWidth() / 2) - 56);
+        this.bhopButtonWidget.setY(3);
+        this.bhopButtonWidget.active = false;
+        this.addSelectableChild(this.bhopButtonWidget);
+
+        this.arenaButtonWidget = ButtonWidget.builder(Text.of("Arena"), (press) -> {
+                    bhop_tab = false;
+                    arena_tab = true;
+                    this.hnsButtonWidget.active = true;
+                    this.hnsListWidget.active = false;
+                    this.bhopButtonWidget.active = true;
+                    this.bhopListWidget.active = false;
+                    this.arenaButtonWidget.active = false;
+                    this.arenaListWidget.active = true;
+                })
+                .size(48, 12)
+                .build();
+
+        this.arenaButtonWidget.setX((this.width / 2) - (this.arenaButtonWidget.getWidth() / 2) + 56);
+        this.arenaButtonWidget.setY(3);
+        this.arenaButtonWidget.active = true;
+        this.addSelectableChild(this.arenaButtonWidget);
+
+        this.hnsButtonWidget = ButtonWidget.builder(Text.of("HNS"), (press) -> {
+                    bhop_tab = false;
+                    arena_tab = false;
+                    this.hnsButtonWidget.active = false;
+                    this.hnsListWidget.active = true;
+                    this.bhopButtonWidget.active = true;
+                    this.bhopListWidget.active = false;
+                    this.arenaButtonWidget.active = true;
+                    this.arenaListWidget.active = false;
+                })
+                .size(48, 12)
+                .build();
+
+        this.hnsButtonWidget.setX((this.width / 2) - (this.hnsButtonWidget.getWidth() / 2));
+        this.hnsButtonWidget.setY(3);
+        this.hnsButtonWidget.active = true;
+        this.addSelectableChild(this.hnsButtonWidget);
+
+        this.bhopListWidget = new MapListWidget(this.client, this.width, this.height - 32, 32, 20);
+        this.arenaListWidget = new MapListWidget(this.client, this.width, this.height - 32, 32, 20);
+        this.hnsListWidget = new MapListWidget(this.client, this.width, this.height - 32, 32, 20);
+        for (DataManager.MapData mapData : Minehop.mapList) {
+            if (!mapData.name.equals("spawn")) {
                 double avgTime = 0;
                 double recordCount = 0;
                 for (DataManager.RecordData personalRecordData : Minehop.personalRecordList) {
-                    if (personalRecordData.map_name.equals(recordData.map_name)) {
+                    if (personalRecordData.map_name.equals(mapData.name)) {
                         recordCount += 1;
                         avgTime += personalRecordData.time;
                     }
                 }
                 recordCount = recordCount == 0 ? 1 : recordCount;
                 avgTime = avgTime / recordCount;
-                this.listWidget.addEntry(recordData, avgTime);
+
+                if (mapData.arena) {
+                    DataManager.RecordData recordData = new DataManager.RecordData(mapData.name, mapData.name, 1000000);;
+                    this.arenaListWidget.addEntry(recordData, avgTime, true);
+                }
+                else if (mapData.hns) {
+                    DataManager.RecordData recordData = new DataManager.RecordData(mapData.name, mapData.name, 1000000);;
+                    this.hnsListWidget.addEntry(recordData, avgTime, true);
+                }
+                else {
+                    DataManager.RecordData recordData = DataManager.getRecord(mapData.name);
+                    if (recordData == null) {
+                        recordData = new DataManager.RecordData(mapData.name, mapData.name, 1000000);
+                    }
+                    this.bhopListWidget.addEntry(recordData, avgTime, false);
+                }
             }
         }
-        this.addSelectableChild(this.listWidget);
+
+        this.bhopListWidget.active = false;
+        this.arenaListWidget.active = false;
+        this.hnsListWidget.active = false;
+
+        this.addSelectableChild(this.bhopListWidget);
+        this.addSelectableChild(this.arenaListWidget);
+        this.addSelectableChild(this.hnsListWidget);
     }
 
     @Override
@@ -57,50 +145,110 @@ public class SelectMapScreen extends Screen {
         String fieldText = this.textFieldWidget.getText();
 
         if (!fieldText.equals(this.lastFieldText)) {
-            MapListWidget newListWidget = new MapListWidget(this.client, this.width, this.height - 32, 32, 20);
+            MapListWidget newBhopListWidget = new MapListWidget(this.client, this.width, this.height - 32, 32, 20);
+            MapListWidget newArenaListWidget = new MapListWidget(this.client, this.width, this.height - 32, 32, 20);
+            MapListWidget newHNSListWidget = new MapListWidget(this.client, this.width, this.height - 32, 32, 20);
             if (!fieldText.equals("")) {
-                for (DataManager.RecordData recordData : Minehop.recordList) {
-                    if (!recordData.map_name.equals("spawn") && recordData.map_name.contains(fieldText)) {
+                for (DataManager.MapData mapData : Minehop.mapList) {
+                    if (!mapData.name.equals("spawn") && mapData.name.contains(fieldText)) {
                         double avgTime = 0;
                         double recordCount = 0;
                         for (DataManager.RecordData personalRecordData : Minehop.personalRecordList) {
-                            if (personalRecordData.map_name.equals(recordData.map_name)) {
+                            if (personalRecordData.map_name.equals(mapData.name)) {
                                 recordCount += 1;
                                 avgTime += personalRecordData.time;
                             }
                         }
                         recordCount = recordCount == 0 ? 1 : recordCount;
                         avgTime = avgTime / recordCount;
-                        newListWidget.addEntry(recordData, avgTime);
+
+                        if (mapData.arena) {
+                            DataManager.RecordData recordData = new DataManager.RecordData(mapData.name, mapData.name, 1000000);;
+                            newArenaListWidget.addEntry(recordData, avgTime, true);
+                        } else if (mapData.hns) {
+                            DataManager.RecordData recordData = new DataManager.RecordData(mapData.name, mapData.name, 1000000);;
+                            newHNSListWidget.addEntry(recordData, avgTime, true);
+                        } else {
+                            DataManager.RecordData recordData = DataManager.getRecord(mapData.name);
+                            if (recordData == null) {
+                                recordData = new DataManager.RecordData(mapData.name, mapData.name, 1000000);;
+                            }
+                            newBhopListWidget.addEntry(recordData, avgTime, false);
+                        }
                     }
                 }
             } else {
-                for (DataManager.RecordData recordData : Minehop.recordList) {
-                    if (!recordData.map_name.equals("spawn")) {
+                for (DataManager.MapData mapData : Minehop.mapList) {
+                    if (!mapData.name.equals("spawn")) {
                         double avgTime = 0;
                         double recordCount = 0;
                         for (DataManager.RecordData personalRecordData : Minehop.personalRecordList) {
-                            if (personalRecordData.map_name.equals(recordData.map_name)) {
+                            if (personalRecordData.map_name.equals(mapData.name)) {
                                 recordCount += 1;
                                 avgTime += personalRecordData.time;
                             }
                         }
                         recordCount = recordCount == 0 ? 1 : recordCount;
                         avgTime = avgTime / recordCount;
-                        newListWidget.addEntry(recordData, avgTime);
+
+                        if (mapData.arena) {
+                            DataManager.RecordData recordData = new DataManager.RecordData(mapData.name, mapData.name, 1000000);;
+                            newArenaListWidget.addEntry(recordData, avgTime, true);
+                        } else if (mapData.hns) {
+                            DataManager.RecordData recordData = new DataManager.RecordData(mapData.name, mapData.name, 1000000);;
+                            newHNSListWidget.addEntry(recordData, avgTime, true);
+                        } else {
+                            DataManager.RecordData recordData = DataManager.getRecord(mapData.name);
+                            if (recordData == null) {
+                                recordData = new DataManager.RecordData(mapData.name, mapData.name, 1000000);;
+                            }
+                            newBhopListWidget.addEntry(recordData, avgTime, false);
+                        }
                     }
                 }
             }
-            this.remove(this.listWidget);
-            this.listWidget = newListWidget;
-            this.addSelectableChild(this.listWidget);
+
+            this.remove(this.bhopListWidget);
+            this.bhopListWidget = newBhopListWidget;
+            this.addSelectableChild(this.bhopListWidget);
+
+            this.remove(this.arenaListWidget);
+            this.arenaListWidget = newArenaListWidget;
+            this.addSelectableChild(this.arenaListWidget);
+
+            this.remove(this.hnsListWidget);
+            this.hnsListWidget = newHNSListWidget;
+            this.addSelectableChild(this.hnsListWidget);
+
             this.lastFieldText = fieldText;
         }
 
-        this.listWidget.render(context, mouseX, mouseY, delta);
+        if (this.bhop_tab && this.bhopListWidget != null) {
+            this.bhopListWidget.render(context, mouseX, mouseY, delta);
+        }
+        else if (this.arena_tab && this.arenaListWidget != null) {
+            this.arenaListWidget.render(context, mouseX, mouseY, delta);
+        }
+        else {
+            if (this.hnsListWidget != null) {
+                this.hnsListWidget.render(context, mouseX, mouseY, delta);
+            }
+        }
 
-        TextRenderer textRenderer = this.client.textRenderer;
-        context.drawCenteredTextWithShadow(textRenderer, "Map Selection", this.width / 2, 4, Formatting.WHITE.getColorValue());
+        if (this.bhopButtonWidget != null) {
+            this.bhopButtonWidget.render(context, mouseX, mouseY, delta);
+        }
+
+        if (this.hnsButtonWidget != null) {
+            this.hnsButtonWidget.render(context, mouseX, mouseY, delta);
+        }
+
+        if (this.arenaButtonWidget != null) {
+            this.arenaButtonWidget.render(context, mouseX, mouseY, delta);
+        }
+
+//        TextRenderer textRenderer = this.client.textRenderer;
+//        context.drawCenteredTextWithShadow(textRenderer, "Map Selection", this.width / 2, 4, Formatting.WHITE.getColorValue());
 
         super.render(context, mouseX, mouseY, delta);
     }
