789358ce78f96017625e5326f8ba9bb38a45754b
Plaaasma <47513553+Plaaasma@users.noreply.github.com>
2024-04-19 01:57:33 -0500

    Added the hide others code back after SOMEBODY accidentally removed it

---
+++ src/client/java/net/nerdorg/minehop/mixin/client/PlayerEntityRendererMixin.java
@@ -0,0 +1,33 @@
+package net.nerdorg.minehop.mixin.client;
+
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.model.*;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.nerdorg.minehop.MinehopClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntityRenderer.class)
+public class PlayerEntityRendererMixin {
+    @Inject(method = "render(Lnet/minecraft/client/network/AbstractClientPlayerEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"), cancellable = true)
+    private void onRender(AbstractClientPlayerEntity abstractClientPlayerEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client != null) {
+            if (client.player != null) {
+                if (MinehopClient.hideOthers && !abstractClientPlayerEntity.getEntityName().equals(client.player.getEntityName())) {
+                    ci.cancel();
+                }
+            }
+        }
+    }
+}
